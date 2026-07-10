439e3c4d01bfeeea685511e4a097826e08fe0fe8
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-07-11 00:06:30 +0600

    add Hero banner section desing left side done Right side need some work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 
 import Navbar from "@/components/NaBar/Navbar";
+import HomePageBanner from "@/components/HeroBanner/HomePageBanner";
 import "./globals.css";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HomePageBanner />
     </>
   );
 }
