7ab7b028efbe41b6ed718a79702e9c8a6568d66c
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-07-10 22:43:57 +0600

    set up basic file and folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <>
+      <header className="w-full border-b bg-white">
+        <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-6">
+          {/* Logo */}
+          <Link href="/" className="flex items-center gap-3">
+            <div className="flex h-11 w-11 items-center justify-center rounded-xl bg-[#5B21B6]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 fill-white"
+                viewBox="0 0 24 24"
+              >
+                <path d="M13 2L4 14h6l-1 8 9-12h-6l1-8z" />
+              </svg>
+            </div>
+
+            <h1 className="text-3xl font-bold">
+              <span className="text-[#4C1D95]">Work</span>
+              <span className="text-[#F59E0B]">hive</span>
+            </h1>
+          </Link>
+
+          {/* Desktop Menu */}
+          <nav className="hidden items-center gap-12 text-lg font-medium text-gray-600 lg:flex">
+            <Link href="#" className="hover:text-[#4C1D95]">
+              Find Talent
+            </Link>
+
+            <Link href="#" className="hover:text-[#4C1D95]">
+              Find Work
+            </Link>
+
+            <Link href="#" className="hover:text-[#4C1D95]">
+              Why Workhive
+            </Link>
+
+            <Link href="#" className="hover:text-[#4C1D95]">
+              Enterprise
+            </Link>
+          </nav>
+
+          {/* Right */}
+          <div className="hidden items-center gap-6 lg:flex">
+            <Link
+              href="#"
+              className="font-semibold text-[#4C1D95] hover:text-[#6D28D9]"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+              Sign in
+            </Link>
+
+            <button className="rounded-2xl bg-[#4C1D95] px-7 py-3 font-semibold text-white transition hover:bg-[#5B21B6]">
+              Join free
+            </button>
+          </div>
+
+          {/* Mobile */}
+          <button
+            // onClick={() => setOpen(!open)}
+            className="lg:hidden"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            {/* <Menu size={30} /> */}
+          </button>
         </div>
-      </main>
-    </div>
+{/* 
+        {open && (
+          <div className="border-t bg-white lg:hidden">
+            <nav className="flex flex-col gap-5 p-6 font-medium text-gray-700">
+              <Link href="#">Find Talent</Link>
+              <Link href="#">Find Work</Link>
+              <Link href="#">Why Workhive</Link>
+              <Link href="#">Enterprise</Link>
+
+              <hr />
+
+              <Link href="#">Sign in</Link>
+
+              <button className="rounded-xl bg-[#4C1D95] py-3 text-white">
+                Join free
+              </button>
+            </nav>
+          </div>
+        )} */}
+      </header>
+    </>
   );
 }
